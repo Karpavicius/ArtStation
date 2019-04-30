95dd599aa197fb9c92e8e57f0090388b706edcb7
Karpavicius <evelinksantos@hotmail.com>
2019-04-29 23:12:30 -0300

    mudando database

--- Art/urls.py
+++ Art/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('', views.MarketPlace, name='MarketPlace'),
-    path('', views.Jobs, name='Jobs'),
+    path('ArtStation', views.post_list, name='post_list'),
+    path('MarketPlace', views.MarketPlace, name='MarketPlace'),
+    path('Jobs', views.Jobs, name='Jobs'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
