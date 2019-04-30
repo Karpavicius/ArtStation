267fc04c9bc1b9285317a2513325fe2336e70caf
Karpavicius <evelinksantos@hotmail.com>
2019-04-29 21:48:27 -0300

    adicionado css

--- Art/views.py
+++ Art/views.py
@@ -5,3 +5,6 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'html/post_list.html', {})
+def MarketPlace(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'html/MarketPlace.html', {})
